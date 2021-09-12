df60b7e53d29beaf7c296e600f6b6ea128408794
Saad19971 <saadmasood76@gmail.com>
2021-09-12 18:46:15 +0500

    initial commit

---
+++ LogIn/SignupViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignupViewController.swift
+//  LogIn
+//
+//  Created by Syed Huzaifa Akbar on 12/09/2021.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func createaccount(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
